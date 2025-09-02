b4b8672d7da58b1832ac3ba7c6dd1ba8639d3949
Bismark <agyarebismark6@gmail.com>
2025-09-02 22:43:06 +0000

    feat: implements products page.... WIP

---
+++ src/pages/Products.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mantine/core";
+import { ContentLayout } from "../components/templates/ContentLayout";
+
+function Products() {
+  return (
+    <>
+      <ContentLayout>
+        <Container size="xl">
+          <h1>Products</h1>
+        </Container>
+      </ContentLayout>
+    </>
+  );
+}
+
+export default Products;

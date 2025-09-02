4e2e3a953646e9648822a14968ec3c38a19f9cbf
Bismark <agyarebismark6@gmail.com>
2025-09-02 22:58:27 +0000

    feat: add all types

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export type ApiErrorDetail = {
+  [key: string]: any;
+};
+
+export type ApiError = {
+  error: {
+    code: string;
+    message: string;
+  };
+  details?: ApiErrorDetail;
+};
+
+export type SignUpType = {
+  first_name: string;
+  last_name: string;
+  password: string;
+  email: string;
+};
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  contact_number: string;
+  location?: string;
+  gender?: string;
+}
+
+export type changePasswordType = {
+  oldPassword: string;
+  newPassword: string;
+};

deae3cf62a1be70f2469ad80a5b0b2c59e471440
Bismark <agyarebismark6@gmail.com>
2025-09-08 00:54:40 +0000

    refactor: improve layout structure in AppLayout component

--- src/components/templates/AppLayout.tsx
+++ src/components/templates/AppLayout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { AppHeader } from "../molecules/AppHeader";
+import AppHeader from "../molecules/AppHeader";
 import { AppFooter } from "../molecules/AppFooter";
 
 function AppLayout({ children }: React.PropsWithChildren) {
   return (
-    <div>
+    <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
       <AppHeader />
-      <main>{children}</main>
+      <main style={{ flex: 1 }}>{children}</main>
       <AppFooter />
     </div>
   );

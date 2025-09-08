c3e73940aa902e25a910a0fc71735ca47199d343
Bismark <agyarebismark6@gmail.com>
2025-09-08 00:56:40 +0000

    feat: add breadcrumb entries for product, cart, favorite, checkout, and orders routes

--- src/utils/router/routes.tsx
+++ src/utils/router/routes.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Home from "../../pages/Home";
 import Products from "../../pages/Products";
+import ProductDetails from "../../pages/ProductDetails";
+import Cart from "../../pages/Cart";
+import Favourite from "../../pages/Favourite";
+import Checkout from "../../pages/Checkout";
+import MyOrders from "../../pages/MyOrders";
+import OrderDetails from "../../pages/OrderDetails";
 
 const routerConfig = [
   {
@@ -14,6 +20,49 @@ const routerConfig = [
     element: <Products />,
     exact: true,
     protected: false,
+    breadcrump: "shop",
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetails />,
+    exact: true,
+    protected: false,
+    breadcrump: "Product Details",
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+    exact: true,
+    protected: false,
+    breadcrump: "Cart",
+  },
+  {
+    path: "/favorite",
+    element: <Favourite />,
+    exact: true,
+    protected: false,
+    breadcrump: "Favourite",
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+    exact: true,
+    protected: false,
+    breadcrump: "Checkout",
+  },
+  {
+    path: "/me/orders",
+    element: <MyOrders />,
+    exact: true,
+    protected: false,
+    breadcrump: "My Orders",
+  },
+  {
+    path: "/me/orders/:id",
+    element: <OrderDetails />,
+    exact: true,
+    protected: false,
+    breadcrump: "My Orders",
   },
 ];
 

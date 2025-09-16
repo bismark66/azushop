6592e02f2d7a62b7e9c61926901e4f1070e43af7
Bismark <agyarebismark6@gmail.com>
2025-09-16 03:25:09 +0000

    refactor: update Favourite component to use wishlist context for dynamic product rendering

--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -1,34 +1,64 @@
 import { Container, Grid } from "@mantine/core";
 import { ContentLayout } from "../components/templates/ContentLayout";
 import ProductCard from "../components/molecules/ProductCard";
-import { productsData } from "../data/productsData";
 
-// For demo, favourite contains one product
-const favouriteProducts = [productsData.find((p) => p.id === 9)].filter(
-  Boolean
-);
+import { useWishlist } from "../utils/contexts/wishlistContext";
+import { useGetProductById } from "../http/product.mutations";
 
 function Favourite() {
+  const { wishlist } = useWishlist();
+
+  // Fetch all wishlist products from backend
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const productQueries = wishlist.map((id) => useGetProductById(String(id)));
+  const isLoading = productQueries.some((q) => q.isLoading);
+  const isError = productQueries.some((q) => q.isError);
+  // Only include products that are not undefined/null
+  const products = productQueries
+    .map((q) => q.data)
+    .filter((p): p is import("../types/product.types").ProductResponse => !!p);
+
   return (
     <ContentLayout hasBanner hasBreadcrumbs bannerText="Favourite">
       <Container size="xl" pt={32} pb={32}>
         <Grid>
-          {favouriteProducts.length === 0
-            ? null
-            : favouriteProducts.map((product) =>
-                product ? (
-                  <Grid.Col key={product.id} span={3}>
-                    <ProductCard
-                      img={product.image}
-                      title={product.title}
-                      description={product.description}
-                      price={product.price}
-                      brand={product.brand}
-                      showDelete
-                    />
-                  </Grid.Col>
-                ) : null
-              )}
+          {wishlist.length === 0 ? (
+            <div style={{ width: "100%", textAlign: "center", padding: 40 }}>
+              No favorite products yet.
+            </div>
+          ) : isLoading ? (
+            <div style={{ width: "100%", textAlign: "center", padding: 40 }}>
+              Loading wishlist...
+            </div>
+          ) : isError ? (
+            <div
+              style={{
+                width: "100%",
+                textAlign: "center",
+                padding: 40,
+                color: "red",
+              }}
+            >
+              Error loading some products.
+            </div>
+          ) : products.length === 0 ? (
+            <div style={{ width: "100%", textAlign: "center", padding: 40 }}>
+              No favorite products found.
+            </div>
+          ) : (
+            products.map((product) => (
+              <Grid.Col key={product._id} span={3}>
+                <ProductCard
+                  img={product.image}
+                  title={product.name}
+                  description={product.description}
+                  price={product.price}
+                  brand={product.brand}
+                  id={product._id}
+                />
+              </Grid.Col>
+            ))
+          )}
         </Grid>
       </Container>
     </ContentLayout>
